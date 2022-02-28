e21c4e585da8c106d8a1dbb50d72a45a3bf179d7
Kawium <kawanse0@gmail.com>
2022-02-28 16:24:53 +0100

    Navbar
    Added navbar

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,11 @@
 import '../App.css';
 import MainPage from '../pages/mainpage';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
+    <Header / >
       <MainPage />
     </div>
   );
