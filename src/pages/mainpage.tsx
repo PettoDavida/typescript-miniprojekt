c362d1baef89cd40e8e943e1352f4627fe7b2d0a
Erikisak <erik.isak@icloud.com>
2022-03-02 13:25:43 +0100

    Searchbar added and design

--- src/pages/mainpage.tsx
+++ src/pages/mainpage.tsx
@@ -1,10 +1,17 @@
+import SearchBar from "../components/SearchBar";
+import Location from "../components/Location";
+import Forecast from "../components/Forecast";
+
 
 function MainPage(){
     return(
         <div>
             <h1>Hello world</h1>
+            <SearchBar/>
+            <Location/>
+            <Forecast/>
         </div>
-    )
+    );
 }
 
 export default MainPage;
