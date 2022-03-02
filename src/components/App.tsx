9e2073f40585ac44b4747e41a9f7b6b7671acd0e
rchtect <90619276+rchtect@users.noreply.github.com>
2022-03-02 17:24:25 +0100

    Update App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,19 @@
 import '../App.css';
 import MainPage from '../pages/mainpage';
 import Header from './Header';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from '../pages/About';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <MainPage />
+    <Router>
+    <Header />
+      <Routes>
+          <Route path="/" element = { <MainPage/> }/>
+          <Route path ="/about" element = { <About/> }/>
+      </Routes>
+    </Router>
     </div>
   );
 }
